e88f0737a6c3858741fac87e63adedbbf88ec7e2
Neermita Bhattacharya <153750505+Neermita18@users.noreply.github.com>
2023-12-24 23:45:44 +0530

    Update Greetings.py

--- Greetings.py
+++ Greetings.py
@@ -6,21 +6,38 @@ def main(page:ft.Page):
     page.bgcolor=ft.colors.BROWN_500
     greetings = ft.Column()
     
-    firstname= ft.TextField(hint_text="enter your first name :)", focused_border_color="white", focused_bgcolor="white")
+    firstname= ft.Ref[ft.TextField]()
     lastname= ft.Ref[ft.TextField]()
     def buttonclicked(e):
-        greetings.controls.append(ft.Text(f"Hello {firstname.value} {lastname.current.value}!! Have a good day, :D"))
-        firstname.value=""
-        lastname.current.value="" 
-        firstname.focus()
-        page.update()
+        
+        if  firstname.current.value==""  and lastname.current.value=="":
+            firstname.current.error_text="No first name given :("    
+            lastname.current.error_text="No last name given :("
+            page.update()
+        else:
+            page.clean()
+            page.add(ft.Column(alignment="center",controls=[ft.Text(f"Hello {firstname.current.value} {lastname.current.value}!! Have a good day, :D", size=30)]))
+            
+            page.add(ft.Row(controls=[ft.Text(value="Greetings, fellow amigos!", color=ft.colors.PURPLE_50, size=15)],alignment=ft.MainAxisAlignment.CENTER,),
+                ft.Image(src=r'C:\Users\91982\Pictures\greetings.jpg', width=1990, height=200, ),
+                ft.TextField(ref= firstname,hint_text="enter your first name :)", focused_border_color="white", focused_bgcolor="white"),
+                ft.TextField(ref=lastname, hint_text="enter your last name :)", focused_border_color="white"),
+                button,
+                greetings,
+                
+            )
+           
+            firstname.current.value=""
+            lastname.current.value="" 
+            firstname.current.focus()
+            page.update()
     
     button= ft.ElevatedButton("Say hello!", on_click=buttonclicked, color=ft.colors.BROWN_900)
     
     page.add(
         ft.Row(controls=[ft.Text(value="Greetings, fellow amigos!", color=ft.colors.PURPLE_50, size=30)],alignment=ft.MainAxisAlignment.CENTER,),
         ft.Image(src=r'C:\Users\91982\Pictures\greetings.jpg', width=1990, height=200, ),
-        firstname,
+        ft.TextField(ref= firstname,hint_text="enter your first name :)", focused_border_color="white", focused_bgcolor="white"),
         ft.TextField(ref=lastname, hint_text="enter your last name :)", focused_border_color="white"),
         button,
         greetings,
